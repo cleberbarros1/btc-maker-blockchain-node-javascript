fb4d44dd99a4ba8060c433fa367eaf274068cee5
Cleber Barros <cleberbarrosgithub@gmail.com>
2022-07-25 23:13:02 -0300

    Repositorio para atender ao desafio de Blockchain - DIO 2022

---
+++ src/createWallet.js
@@ -0,0 +1,31 @@
+//importando dependencias
+const bip32 = require('bip32')
+const bip39 = require('bip39')
+const bitcoin = require('bitcoinjs-lib')
+
+//definir a rede testet rede de testes e bitcoi main net (rede real)
+const network = bitcoin.networks.testnet
+
+//camiho de derivacao para carteiras HD
+const path = `m/49'/1'/0'/0`
+
+//criando as palavras mnemonic para as seed palavras de senha
+let mnemonic = bip39.generateMnemonic()
+const seed = bip39.mnemonicToSeedSync(mnemonic)
+
+//criando a raiz da carteira HD
+let root = bip32.fromSeed(seed, network)
+
+//criando uma conta - par de pvt pub keys
+let account = root.derivePath(path)
+let node = account.derive(0).derive(0)
+
+let btcAddress = bitcoin.payments.p2pkh({
+    pubkey: node.publicKey,
+    network: network,
+}).address
+
+console.log("carteira gerada")
+console.log("Endereço: ", btcAddress)
+console.log("Chave privada: ", node.toWIF())
+console.log("Seed: ", mnemonic)
